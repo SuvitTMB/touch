92108dad9f1908ca13498c7e9dd49deb4034e2c3
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-06-20 18:12:25 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ var CheckFoundData = 0;
 
 
 $(document).ready(function () {
+	/*
   sessionStorage.clear(); 
   var str = "";
   var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
@@ -19,15 +20,16 @@ $(document).ready(function () {
   str += '<div class="NameLine">'+ sessionStorage.getItem("LineName")+'</div>';
   $("#MyProfile").html(str);  
   Connect_DB();
-  
+  */
 
-  //main();
+
+  main();
 });
 
 
 
 async function main() {
-  await liff.init({ liffId: "1655966947-l6e0adNj" });
+  await liff.init({ liffId: "1655966947-r3ZbqoGK" });
   document.getElementById("isLoggedIn").append(liff.isLoggedIn());
   if(liff.isLoggedIn()) {
     getUserProfile();
